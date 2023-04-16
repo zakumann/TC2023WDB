5447dd517abb403e6828ae92b8c93d249ca0d36c
zakumann <samuel742able@gmail.com>
2023-04-16 22:32:15 +0900

    Add toUpperCase().js

---
+++ toUpperCase().js
@@ -0,0 +1,13 @@
+var name = prompt("What is your name?");
+
+var firstChar = name.slice(0,1);
+
+var upperCaseFirstChar = firstChar.toUpperCase();
+
+var restOfName = name.slice(1,name.length);
+
+restOfName = restOfName.toLowerCase();
+
+var capitalisedName = upperCaseFirstChar + restOfName;
+
+alert("Hello, " + capitalisedName);
