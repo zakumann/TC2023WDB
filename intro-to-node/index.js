fa3be53a9ece0dfc730dccd275c3d0e61c08ab77
zakumann <samuel742able@gmail.com>
2023-05-08 22:35:10 +0900

    complete section 23

--- intro-to-node/index.js
+++ intro-to-node/index.js
@@ -1,1 +1,10 @@
-console.log("Hello world!");
+//jshint esversion:6
+
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+
+var mySuperheroName = superheroes.random();
+var mySupervillains = supervillains.random()
+
+console.log(mySuperheroName)
+console.log(mySupervillains);
