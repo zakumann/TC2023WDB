da12c685e2c74fab3b69c3fa113eafc491f44eb3
zakumann <samuel742able@gmail.com>
2023-05-10 22:23:23 +0900

    Create calculator

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,9 +1,24 @@
+
+
 const express = require('express');
+
 const app = express();
 
 app.get("/", function(req, res){
     res.send("<h1>Hello World!</h1>");
 });
+
+app.get("/contact", function(req, res){
+    res.send("Contact me at: samuel742able@gmail.com");
+});
+
+app.get("/about", function(req, res){
+    res.send("My name is Samuel and I love leaning English and typing code.");
+});
+
+app.get("/hobbies", function(req, res){
+    res.send("<ul><li>Typing Code</li><li>Leaning English</li></ul>")
+})
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
