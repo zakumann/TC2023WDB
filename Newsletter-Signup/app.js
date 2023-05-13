8e0fff13df003f1d0dcce3dd9d45d8bdb9d2744a
zakumann <samuel742able@gmail.com>
2023-05-13 19:05:18 +0900

    13052023

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,9 +1,9 @@
-
+//jshint esversion: 6
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require("request");
-
+// const request = require("request");
+const https = require("https");
 
 const app = express();
 
@@ -16,15 +16,60 @@ app.get("/", function(req, res){
 
 app.post("/", function(req, res){
 
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var email = req.body.email;
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
 
-    console.log(firstName, lastName, email);
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName,
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data)
+
+    const url = "3aa92829fbf84fe3e4fe1fb271613d6b-us21";
+
+    const options = {
+        method: "POST",
+        auth: "samuel:3aa92829fbf84fe3e4fe1fb271613d6b-us21"
+        }
+
+    const request = https.request(url, options, function(response){
+
+            if (response.statusCode === 200){
+                res.sendFile(__dirname + "/success.html");
+            } else {
+                res.sendFile(__dirname + "/failure.html");
+            }
+
+            response.on("data", function(data){
+                console.log(JSON.parse(data));
+            });
+        });
+
+        request.write(jsonData);
+        request.end();
+
+    });
 
     
-});
+    app.post("/failure", function(req, res){
+        res.redirect("/");
+    })
+
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
 });
+
+
+// api key
+// 3aa92829fbf84fe3e4fe1fb271613d6b-us21
