8452cd0dd70a8e8a6ad9074dfede3d4f6a068e15
zakumann <samuel742able@gmail.com>
2023-05-17 22:37:15 +0900

    05172023

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -6,44 +6,36 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
 
 var today = new Date();
-var currentDay = today.getDay();
-var day = "";
 
-switch(currentDay){
-    case 0:
-        day = "Sunday";
-    break;
-    case 1:
-        day = "Monday";
-    break;
-    case 2:
-        day = "Tuesday";
-    break;
-    case 3:
-        day = "Wednesday";
-    break;
-    case 4:
-        day = "Thursday";
-    break;
-    case 5:
-        day = "Friday";
-    break;
-    case 6:
-        day = "Saturday";
-    break;
-    default:
-        console.log("Error: current day is equal to : " + currentDay);
-}
+var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+};
 
-    res.render("list", {
-        kindOfDay: day
-    });
+    var day = today.toLocaleDateString("en-US", options);
+
+
+    res.render("list", {kindOfDay: day, newListItems: items});
     
+});
+
+app.post("/", function(req, res){
+    var item = req.body.newItem;
+
+    items.push(item);
+    
+    res.redirect("/");
+
 });
 
 
