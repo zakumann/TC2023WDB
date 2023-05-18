bd9edb8ca664fc02a5a6e02aa7e7b3e1d4320ffd
zakumann <samuel742able@gmail.com>
2023-05-18 22:04:25 +0900

    add about, footer, header ejs files

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -6,11 +6,13 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
 
@@ -24,21 +26,37 @@ var options = {
 
     var day = today.toLocaleDateString("en-US", options);
 
-
-    res.render("list", {kindOfDay: day, newListItems: items});
+    res.render("list", {listTitle: day, newListItems: items});
     
 });
 
 app.post("/", function(req, res){
+
     var item = req.body.newItem;
 
-    items.push(item);
-    
-    res.redirect("/");
+    if (req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
 
 });
 
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
 
+app.get("/about", function(req, res){
+    res.render("about");
+});
+
+app.post("/work", function(req, res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+});
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
