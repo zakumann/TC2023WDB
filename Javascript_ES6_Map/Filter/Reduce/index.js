e54e765e299141dbf17742cdd6d20e06c2ec6885
zakumann <samuel742able@gmail.com>
2023-06-02 22:53:01 +0900

    javascript ES6 map/Filter/Reduce

---
+++ Javascript_ES6_Map/Filter/Reduce/index.js
@@ -0,0 +1,61 @@
+// var numbers = [3, 56, 2, 48, 5];
+
+// function double(x){
+//     return x * 2;
+// }
+
+// const newNumbers = numbers.map(double);
+
+// var newNumbers = [];
+
+// Map
+
+// const newNumbers = numbers.map(function (x){
+//     return x * 2;
+// });
+
+// console.log(newNumbers);
+
+// var numbers = [3, 56, 2, 48, 5];
+// Filter
+
+// const newNumbers = numbers.filter(function (num){
+//     return num > 10
+// });
+
+// var newNumbers = [];
+// numbers.forEach(function(num){
+//     if (num < 10){
+//         newNumbers.push(num);
+//     }
+// })
+
+// console.log(newNumbers);
+
+// var numbers = [3, 56, 2, 48, 5];
+//Reduce
+// var newNumber = numbers.reduce(function(accumulator, currentNumber){
+//     console.log("accumulator = " + accumulator);
+//     console.log("currentNumber = " + currentNumber);
+//     return accumulator + currentNumber;
+// })
+
+// var newNumber = 0;
+// numbers.forEach(function(currentNumber){
+//     newNumber += currentNumber
+// })
+var numbers = [3, 56, 2, 48, 5];
+
+
+//Find
+
+// const newNumber = numbers.find(function(num){
+//     return num > 10;
+// })
+
+// Findindex
+// const newNumber = numbers.findIndex(function (num){
+//     return num > 10;
+// })
+
+// console.log(newNumber);
